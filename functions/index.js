a8a001b5da4ddc9e6de8d321469c29549301f632
Jonathan Song <jonsong2013@yahoo.com>
2020-09-28 00:51:31 -0400

    Converted static files to ejs

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,37 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('*', (req, res) => {
-  res.render('main');
+
+app.get('/', (req, res) => {
+  res.render('main', {activePage: "Home"});
 });
+
+const tags = {"politics": "Politics", "environment": "Environment", "covid": "COVID-19", "demographics": "Demographics"};
+const authors = {"author_one": "Author One"};
+
+function getActivePage(category){
+  if(category in tags){
+    return tags[category];
+  }else if(category in authors){
+      return "Home";
+  }
+}
+
+app.get('/article', (req, res) => {
+  res.render('article', {
+    activePage: "Home"
+  });
+});
+
+app.get('/:category', (req, res) => {
+  let category = req.params.category;
+  res.render('main', {
+    activePage: getActivePage(category)
+  });
+
+
+});
+
+
 
 exports.app = functions.https.onRequest(app);
