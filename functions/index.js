1ad96ccafa4f9a82f92ea5d7e10cf1485d9c5674
Jonathan Song <jonsong2013@yahoo.com>
2020-10-08 23:27:54 -0400

    Retrieve article from database

--- functions/index.js
+++ functions/index.js
@@ -3,39 +3,78 @@ const admin = require('firebase-admin');
 
 const express = require('express');
 const app = express();
-
 app.set('view engine', 'ejs');
+
+const serviceAccount = require('./serviceAccount.json');
+
+const moment = require('moment');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://stanford-data-journalism.firebaseio.com"
+});
 
 
 app.get('/', (req, res) => {
   res.render('main', {activePage: "Home"});
 });
 
-const tags = {"politics": "Politics", "environment": "Environment", "covid": "COVID-19", "demographics": "Demographics"};
+const tagNames = {"politics": "Politics", "environment": "Environment", "covid": "COVID-19", "demographics": "Demographics"};
 const authors = {"author_one": "Author One"};
 
 function getActivePage(category){
-  if(category in tags){
-    return tags[category];
+  if(category in tagNames){
+    return tagNames[category];
   }else if(category in authors){
       return "Home";
   }
 }
 
-app.get('/article', (req, res) => {
-  res.render('article', {
-    activePage: "Home"
-  });
-});
-
-app.get('/:category', (req, res) => {
+app.get('/:category', (req, res, next) => {
   let category = req.params.category;
-  res.render('main', {
-    activePage: getActivePage(category)
-  });
 
-
+  if(category in tagNames || category in authors){
+    res.render('main', {
+      activePage: getActivePage(category)
+    });
+  }else {
+    next();
+  }
 });
+
+app.get('/:article', (req, res, next) => {
+  let id = req.params.article;
+
+  admin.firestore().collection("articles").doc(id).get().then(snap => {
+    let article = snap.data();
+    let time = new moment(article.timestamp.seconds*1000);
+    let author_id = article.author.toLowerCase().split().join("_");
+    let tags = article.tags.map(tag => {
+      console.log(tagNames[tag]);
+      return {tagID: tagNames[tag], tagName: tag}});
+
+    res.render('article', {
+      title: article.title,
+      subtitle: article.subtitle,
+      image: article.image,
+      date: time.format('MMMM Do, YYYY'),
+      author: article.author,
+      author_id: author_id,
+      tags: tags,
+      text: article.text
+    });
+
+  }).catch((err) => {
+    console.log(err);
+    next();
+  })
+});
+
+app.get('*', (req, res) => {
+  res.render('404');
+});
+
+
 
 
 
